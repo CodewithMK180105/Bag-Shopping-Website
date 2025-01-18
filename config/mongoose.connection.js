13bf7b5a519d8c3e08a35e914e0610a5c547ab4a
CodewithMK180105 <manishkumar180105@gmail.com>
2025-01-18 20:49:58 +0530

    Config and debug set

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,12 +1,14 @@
 const mongoose= require('mongoose');
+const dbgr= require("debug")("development:mongoose");
+const config= require('config');
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/BagsSellingWebsite")
+.connect(`${config.get("MONGODB_URI")}/BagsSellingWebsite`)
 .then(()=>{
-    console.log("Connected")
+    dbgr("Connected")
 })
 .catch((err)=>{
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports= mongoose.connection;

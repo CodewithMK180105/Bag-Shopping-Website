6f243eec0b519bd0c9568f9600abee377157fbff
CodewithMK180105 <manishkumar180105@gmail.com>
2025-01-26 14:12:19 +0530

    Implemented the profile and the cart route and their respected UI

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,32 @@ const express=require("express");
 const router=express.Router();
 const isloggedIn=require("../middlewares/isLoggedIn");
 const productModels = require("../models/product.models");
+const userModels = require("../models/user.models");
 
 router.get("/", (req,res)=>{
     const error=req.flash("error");
-    res.render("index", {error});
+    res.render("index", {error, loggedin: false});
 });
 
 router.get("/shop", isloggedIn, async (req, res)=>{
     let products=await productModels.find();
-    res.render("shop",{products});
+    let success=req.flash("success");
+    res.render("shop",{products, success});
+})
+router.get("/cart", isloggedIn, async (req, res)=>{
+    let user=await userModels.findOne({email: req.user.email}).populate("cart");
+    res.render("cart",{user});
+})
+router.get("/profile", isloggedIn, async (req, res)=>{
+    let user=await userModels.findOne({email: req.user.email});
+    res.render("profile",{user});
+})
+router.get("/addtocart/:productid", isloggedIn, async (req, res)=>{
+    let user= await userModels.findOne({email: req.user.email});
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect('/shop');
 })
 
 module.exports= router;
